1b17d359219e32ff496dbd78b8bab5acfff7571c
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2024-12-02 22:25:27 +0000

    Solution day 2, part 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,8 +1,20 @@
-fn valid_report(report: &[i32]) -> bool {
+fn is_safe_report(report: &[i32]) -> bool {
     let mut diffs = report.windows(2).map(|pair| pair[1] - pair[0]);
     let signum = (report[1] - report[0]).signum();
 
     diffs.all(|diff| diff.abs() >= 1 && diff.abs() <= 3 && diff.signum() == signum)
+}
+
+fn is_safe_problem_damped_report(report: &[i32]) -> bool {
+    if is_safe_report(report) {
+        return true;
+    }
+
+    (0..report.len()).any(|index| {
+        let mut new_report = report.to_vec();
+        new_report.remove(index);
+        is_safe_report(&new_report)
+    })
 }
 
 fn main() {
@@ -16,7 +28,19 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let safe_reports = reports.iter().filter(|report| valid_report(report)).count();
+    let safe_reports = reports
+        .iter()
+        .filter(|report| is_safe_report(report))
+        .count();
 
-    println!("There are {} safe levels.", safe_reports);
+    println!("There are {} safe reports.", safe_reports);
+
+    let safe_problem_damped_reports = reports
+        .iter()
+        .filter(|report| is_safe_problem_damped_report(report))
+        .count();
+    println!(
+        "There are {} safe problem damped reports.",
+        safe_problem_damped_reports
+    );
 }
