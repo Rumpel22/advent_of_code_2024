0464d5e394cc4fcccde6b621d3b72a9eff342a74
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2024-12-10 21:33:57 +0000

    Solution day 6, part 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,7 +1,11 @@
 use std::collections::HashSet;
 
+use rayon::prelude::*;
+
 struct Map {
     data: &'static str,
+    height: i32,
+    width: i32,
 }
 
 #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -11,6 +15,13 @@ struct Coordinates {
 }
 
 impl Map {
+    fn new(data: &'static str) -> Self {
+        Self {
+            data,
+            height: data.lines().count() as i32,
+            width: data.find('\n').unwrap() as i32,
+        }
+    }
     fn start_position(&self) -> Coordinates {
         self.data
             .lines()
@@ -24,19 +35,16 @@ impl Map {
             .next()
             .unwrap()
     }
-    fn height(&self) -> i32 {
-        self.data.lines().count() as i32
-    }
-    fn width(&self) -> i32 {
-        self.data.find('\n').unwrap() as i32
-    }
     fn is_blocked(&self, coordinates: &Coordinates) -> bool {
-        let index = (coordinates.x + coordinates.y * (self.width() + 1)) as usize;
+        let index = (coordinates.x + coordinates.y * (self.width + 1)) as usize;
         self.data.bytes().nth(index) == Some(b'#')
+    }
+    fn on_map(&self, coordinates: &Coordinates) -> bool {
+        (0..self.width).contains(&coordinates.x) && (0..self.height).contains(&coordinates.y)
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Direction {
     Up,
     Down,
@@ -78,29 +86,85 @@ impl Coordinates {
     }
 }
 
-fn main() {
-    let input = include_str!("../input/input.txt");
-    let map = Map { data: input };
-    let mut position = map.start_position();
-    let mut direction = Direction::Up;
-    let mut positions = HashSet::new();
-    loop {
-        println!("{:?}", position);
+fn unique_coordinates(positions: &[Coordinates]) -> HashSet<Coordinates> {
+    HashSet::from_iter(positions.iter().cloned())
+}
 
-        positions.insert(position);
-        let new_position = position.go(&direction);
-        if map.is_blocked(&new_position) {
-            direction = direction.turn_right();
-            println!("New direction: {:?}", direction);
-        } else {
-            position = new_position;
-            if !(0..map.width()).contains(&position.x) || !(0..map.height()).contains(&position.y) {
-                break;
-            }
+fn take_step(
+    map: &Map,
+    position: &Coordinates,
+    direction: &Direction,
+) -> Option<(Coordinates, Direction)> {
+    let mut new_position = position.go(&direction);
+    let mut new_direction = *direction;
+    if map.is_blocked(&new_position) {
+        new_position = *position;
+        new_direction = direction.turn_right();
+    } else {
+        if !map.on_map(&new_position) {
+            return None;
         }
     }
+    Some((new_position, new_direction))
+}
+
+fn walk(map: &Map, start: &Coordinates, direction: &Direction) -> Vec<Coordinates> {
+    let mut position = *start;
+    let mut direction = *direction;
+    let mut positions = vec![];
+    loop {
+        positions.push(position);
+        if let Some((new_position, new_direction)) = take_step(map, &position, &direction) {
+            position = new_position;
+            direction = new_direction;
+        } else {
+            break;
+        }
+    }
+    positions
+}
+
+fn main() {
+    let input = include_str!("../input/input.txt");
+    let map = Map::new(input);
+    let start = map.start_position();
+
+    let positions = walk(&map, &start, &Direction::Up);
+    let unique_positions = unique_coordinates(&positions);
     println!(
         "The guard visits {} positions before leaving the mapped area.",
-        positions.len()
+        unique_positions.len()
+    );
+
+    let block_positions = positions
+        .iter()
+        .skip(1)
+        .filter(|block| {
+            let mut turning_points = HashSet::new();
+            let mut position = start;
+            let mut direction = Direction::Up;
+
+            loop {
+                let new_position = position.go(&direction);
+                if !map.on_map(&new_position) {
+                    return false;
+                }
+                if map.is_blocked(&new_position) || new_position == **block {
+                    if !turning_points.insert((position, direction)) {
+                        return true;
+                    }
+                    direction = direction.turn_right();
+                } else {
+                    position = new_position;
+                }
+            }
+        })
+        .cloned()
+        .collect::<Vec<_>>();
+
+    let unique_block_positions = unique_coordinates(&block_positions);
+    println!(
+        "There are {} different positions to place an obstruction",
+        unique_block_positions.len()
     );
 }
