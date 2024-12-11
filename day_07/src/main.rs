66a75bb9346712b29a02d815ef7985d3eecb7f7d
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2024-12-11 22:25:17 +0000

    Solution day 7, part 2
    The solution required some changes also for part 1, but should still work.

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -4,23 +4,42 @@ struct Equation {
     numbers: Vec<i64>,
 }
 
+fn concat(a: i64, b: i64) -> i64 {
+    let x = (b as f64).log10();
+    let x = x as u32;
+    a * 10i64.pow(x + 1) + b
+}
+
 fn possible_equation(result: i64, numbers: &[i64]) -> bool {
     if numbers.len() == 1 {
         return result == numbers[0];
     }
 
-    let last = numbers.last().unwrap();
-    let rest = &numbers[..numbers.len() - 1];
+    let mut new_numbers = vec![numbers[0] + numbers[1]];
+    new_numbers.extend_from_slice(&numbers[2..]);
 
-    possible_equation(result - last, rest)
-        || (result % last == 0 && possible_equation(result / last, rest))
+    if possible_equation(result, &new_numbers) {
+        return true;
+    }
+
+    new_numbers[0] = numbers[0] * numbers[1];
+    if possible_equation(result, &new_numbers) {
+        return true;
+    }
+
+    new_numbers[0] = concat(numbers[0], numbers[1]);
+    if possible_equation(result, &new_numbers) {
+        return true;
+    }
+    false
 }
 
 fn possible(equation: &&Equation) -> bool {
     possible_equation(equation.result, &equation.numbers)
 }
+
 fn main() {
-    let input = include_str!("../input/input.txt");
+    let input = include_str!("../input/demo.txt");
     let equations = input
         .lines()
         .map(|line| {
@@ -37,7 +56,6 @@ fn main() {
     let total_calibration_result = equations
         .iter()
         .filter(possible)
-        .inspect(|e| println!("{:?}", e))
         .map(|equation| equation.result)
         .sum::<i64>();
     println!(
