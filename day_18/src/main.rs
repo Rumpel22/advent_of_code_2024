4ae5358aebaf099eb4a20ee30306a1f12137621d
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2025-01-02 23:00:33 +0000

    Solution day 18, part 2 (slow)

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, iter};
+use std::{collections::HashMap, fmt::Display, iter};
 
 enum Direction {
     Left,
@@ -11,6 +11,12 @@ enum Direction {
 struct XY {
     x: u8,
     y: u8,
+}
+
+impl Display for XY {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
+    }
 }
 struct Map {
     corrupted: Vec<XY>,
@@ -74,8 +80,8 @@ fn parse_input(input: &str) -> Vec<XY> {
     bytes
 }
 
-fn h(position: &XY, end: &XY) -> u8 {
-    position.x.abs_diff(end.x) + position.y.abs_diff(end.y)
+fn h(position: &XY, end: &XY) -> u16 {
+    (position.x.abs_diff(end.x) + position.y.abs_diff(end.y)) as u16
 }
 
 fn reconstruct_path(came_from: &HashMap<XY, XY>, current: &XY) -> Vec<XY> {
@@ -84,7 +90,7 @@ fn reconstruct_path(came_from: &HashMap<XY, XY>, current: &XY) -> Vec<XY> {
         .collect()
 }
 
-fn find_path(map: &Map) -> Vec<XY> {
+fn find_path(map: &Map) -> Option<Vec<XY>> {
     let start = XY { x: 0, y: 0 };
     let end = XY {
         x: map.width - 1,
@@ -98,32 +104,48 @@ fn find_path(map: &Map) -> Vec<XY> {
 
     while let Some(current) = open_set.pop() {
         if current == end {
-            return reconstruct_path(&came_from, &current);
+            return Some(reconstruct_path(&came_from, &current));
         }
 
+        let mut inserted = false;
         for neighbor in map.neighbors(&current) {
             let tentative_g_score = g_score.get(&current).unwrap() + 1;
-            if tentative_g_score < *g_score.get(&neighbor).unwrap_or(&u8::MAX) {
+            if tentative_g_score < *g_score.get(&neighbor).unwrap_or(&u16::MAX) {
                 came_from.insert(neighbor, current);
                 g_score.insert(neighbor, tentative_g_score);
                 f_score.insert(neighbor, tentative_g_score + h(&neighbor, &end));
                 if !open_set.contains(&neighbor) {
                     open_set.push(neighbor);
+                    inserted = true;
                 }
             }
         }
-        open_set.sort_unstable_by_key(|x| f_score.get(x).unwrap());
-        open_set.reverse();
+        if inserted {
+            open_set.sort_unstable_by_key(|x| f_score.get(x).unwrap());
+            open_set.reverse();
+        }
     }
-    unreachable!()
+    None
 }
 
 fn main() {
     let input = include_str!("../input/input.txt");
     let bytes = parse_input(input);
     let nb_bytes = if bytes.len() < 100 { 12 } else { 1024 };
-    let map = Map::new(&bytes[..nb_bytes]);
+    let mut map = Map::new(&bytes[..nb_bytes]);
 
     let path = find_path(&map);
-    println!("The shortest path is {} long.", path.len() - 1);
+    println!(
+        "The shortest path after {} fallen bytes is {} steps long.",
+        nb_bytes,
+        path.unwrap().len() - 1
+    );
+
+    for byte in &bytes[nb_bytes..] {
+        map.corrupted.push(*byte);
+        if find_path(&map).is_none() {
+            println!("After byte {}, the end is not reachable anymore.", byte);
+            break;
+        }
+    }
 }
