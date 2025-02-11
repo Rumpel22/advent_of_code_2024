a171d8429603673db9a5b935e4e3207fd64cc07c
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2025-02-11 20:17:37 +0000

    Finally solution for day 21, part 1

--- day_21/src/main.rs
+++ day_21/src/main.rs
@@ -1,140 +1,14 @@
-use std::{cell::RefCell, iter, marker::PhantomData};
+use std::{fmt::Debug, iter};
 
 use itertools::Itertools;
 
-struct Keypad<T>
-where
-    T: KeypadButton,
-{
-    _s: PhantomData<T>,
-}
-trait KeypadButton {
-    fn from_to(from: &Self, to: &Self) -> Command;
-    fn go(&self, command: &Command) -> Self;
-    fn start() -> Self;
-}
-
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Clone, Copy, Debug)]
 enum NumericKeypadButton {
     Digit(u8),
     Activate,
 }
 
-impl NumericKeypadButton {
-    fn col(&self) -> u8 {
-        match self {
-            NumericKeypadButton::Digit(0) => 1,
-            NumericKeypadButton::Digit(digit) => (digit + 2) % 3,
-            NumericKeypadButton::Activate => 2,
-        }
-    }
-    fn row(&self) -> u8 {
-        match self {
-            NumericKeypadButton::Digit(digit) => (digit + 2) / 3,
-            NumericKeypadButton::Activate => 0,
-        }
-    }
-}
-
-impl KeypadButton for NumericKeypadButton {
-    fn from_to(from: &Self, to: &Self) -> Command {
-        let from_col = from.col();
-        let from_row = from.row();
-        let to_col = to.col();
-        let to_row = to.row();
-        if to_row > from_row {
-            return Command::Up;
-        }
-        if to_col > from_col {
-            return Command::Right;
-        }
-        if to_col < from_col {
-            return Command::Left;
-        }
-        if to_row < from_row {
-            return Command::Down;
-        }
-        return Command::A;
-    }
-
-    fn go(&self, command: &Command) -> Self {
-        match (command, self) {
-            (Command::Up, Self::Digit(0)) => Self::Digit(2),
-            (Command::Up, Self::Digit(digit)) => Self::Digit(digit + 3),
-            (Command::Up, Self::Activate) => Self::Digit(3),
-            (Command::Down, Self::Digit(2)) => Self::Digit(0),
-            (Command::Down, Self::Digit(3)) => Self::Activate,
-            (Command::Down, Self::Digit(digit)) => Self::Digit(digit - 3),
-            (Command::Left, Self::Activate) => Self::Digit(0),
-            (Command::Left, Self::Digit(digit)) => Self::Digit(digit - 1),
-            (Command::Right, Self::Digit(0)) => Self::Activate,
-            (Command::Right, Self::Digit(digit)) => Self::Digit(digit + 1),
-            (Command::A, button) => button.clone(),
-            _ => unreachable!(),
-        }
-    }
-
-    fn start() -> Self {
-        Self::Activate
-    }
-}
-
-impl KeypadButton for Command {
-    fn from_to(from: &Self, to: &Self) -> Command {
-        match (from, to) {
-            (command1, command2) if command1 == command2 => Command::A,
-            (Command::Up, Command::A) => Command::Right,
-            (Command::Up, _) => Command::Down,
-            (Command::A, Command::Up) => Command::Left,
-            (Command::A, _) => Command::Down,
-            (Command::Left, _) => Command::Right,
-            (Command::Right, Command::Left) | (Command::Right, Command::Down) => Command::Left,
-            (Command::Right, _) => Command::Up,
-            (Command::Down, Command::Up) => Command::Up,
-            (Command::Down, Command::Left) => Command::Left,
-            (Command::Down, _) => Command::Right,
-        }
-    }
-
-    fn go(&self, command: &Command) -> Self {
-        match (self, command) {
-            (command, Command::A) => *command,
-            (Command::Up, Command::Down) => Command::Down,
-            (Command::Up, Command::Right) => Command::A,
-            (Command::Down, command) => *command,
-            (Command::Left, Command::Right) => Command::Down,
-            (Command::Right, Command::Up) => Command::A,
-            (Command::Right, Command::Left) => Command::Down,
-            (Command::A, Command::Down) => Command::Right,
-            (Command::A, Command::Left) => Command::Up,
-            _ => unreachable!(),
-        }
-    }
-
-    fn start() -> Self {
-        Command::A
-    }
-}
-
-impl<T: KeypadButton> Keypad<T> {
-    fn enter_code(code: &[T]) -> Vec<Command> {
-        let key = RefCell::new(T::start());
-        code.iter()
-            .flat_map(|target| {
-                iter::repeat(target)
-                    .map(|target| {
-                        let command = T::from_to(&key.borrow(), target);
-                        let next_key = key.borrow().go(&command);
-                        *key.borrow_mut() = next_key;
-                        command
-                    })
-                    .take_while_inclusive(|command| command != &Command::A)
-            })
-            .collect::<Vec<_>>()
-    }
-}
-
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Clone, Copy, Debug)]
 enum Command {
     Up,
     Down,
@@ -143,20 +17,141 @@ enum Command {
     A,
 }
 
-fn to_string(commands: &[Command]) -> String {
-    commands
-        .iter()
-        .map(|command| match command {
-            Command::Up => '^',
-            Command::Down => 'v',
-            Command::Left => '<',
-            Command::Right => '>',
-            Command::A => 'A',
-        })
-        .collect()
+trait Button: PartialEq + Copy + Debug {
+    fn go(&self, command: &Command) -> Option<Self>
+    where
+        Self: Sized;
+    fn start() -> Self;
 }
 
-fn complexity(commands: &[Command], code: &[NumericKeypadButton]) -> usize {
+impl Command {
+    fn options() -> impl Iterator<Item = Command> {
+        iter::successors(Some(Command::A), |direction| match direction {
+            Command::A => Some(Command::Up),
+            Command::Up => Some(Command::Down),
+            Command::Down => Some(Command::Left),
+            Command::Left => Some(Command::Right),
+            Command::Right => None,
+        })
+    }
+}
+
+impl Button for NumericKeypadButton {
+    fn go(&self, command: &Command) -> Option<Self> {
+        match (command, self) {
+            (Command::Up, Self::Digit(0)) => Some(Self::Digit(2)),
+            (Command::Up, Self::Digit(digit)) if *digit <= 6 => Some(Self::Digit(digit + 3)),
+            (Command::Up, Self::Activate) => Some(Self::Digit(3)),
+            (Command::Down, Self::Digit(2)) => Some(Self::Digit(0)),
+            (Command::Down, Self::Digit(3)) => Some(Self::Activate),
+            (Command::Down, Self::Digit(digit)) if *digit >= 4 => Some(Self::Digit(digit - 3)),
+            (Command::Left, Self::Activate) => Some(Self::Digit(0)),
+            (Command::Left, Self::Digit(0)) => None,
+            (Command::Left, Self::Digit(digit)) if (digit - 1) % 3 >= 1 => {
+                Some(Self::Digit(digit - 1))
+            }
+            (Command::Right, Self::Digit(0)) => Some(Self::Activate),
+            (Command::Right, Self::Activate) => None,
+            (Command::Right, Self::Digit(digit)) if *digit % 3 >= 1 => Some(Self::Digit(digit + 1)),
+            (Command::A, button) => Some(*button),
+            _ => None,
+        }
+    }
+    fn start() -> Self {
+        NumericKeypadButton::Activate
+    }
+}
+
+impl Button for Command {
+    fn go(&self, command: &Command) -> Option<Self>
+    where
+        Self: Sized,
+    {
+        match (self, command) {
+            (command, Command::A) => Some(*command),
+            (Command::Up, Command::Down) => Some(Command::Down),
+            (Command::Up, Command::Right) => Some(Command::A),
+            (Command::Down, command) if *command != Command::Down => Some(*command),
+            (Command::Left, Command::Right) => Some(Command::Down),
+            (Command::Right, Command::Up) => Some(Command::A),
+            (Command::Right, Command::Left) => Some(Command::Down),
+            (Command::A, Command::Down) => Some(Command::Right),
+            (Command::A, Command::Left) => Some(Command::Up),
+            _ => None,
+        }
+    }
+
+    fn start() -> Self {
+        Command::A
+    }
+}
+
+fn from_to<T: Button>(from: &T, to: &T) -> Vec<Vec<Command>> {
+    let mut possibilities: Vec<Vec<Command>> = vec![];
+    let mut options = vec![(*from, vec![])];
+
+    while let Some((current, path)) = options.pop() {
+        if let Some(first) = possibilities.first() {
+            if path.len() > first.len() {
+                break;
+            }
+        }
+        for (option, command) in Command::options()
+            .filter_map(|command| current.go(&command).and_then(|next| Some((next, command))))
+        {
+            let mut new_path = path.clone();
+            new_path.push(command);
+            if option == *to {
+                if command != Command::A {
+                    new_path.push(Command::A);
+                }
+                possibilities.push(new_path);
+            } else {
+                options.push((option, new_path));
+            }
+        }
+        options.sort_by_key(|(_, path)| path.len());
+        options.reverse();
+    }
+
+    possibilities.into_iter().min_set_by_key(|x| x.len())
+}
+
+trait Keypad {
+    fn cost<T: Button>(&self, path: &[T]) -> usize;
+}
+
+struct DirectInput;
+
+impl Keypad for DirectInput {
+    fn cost<T: Button>(&self, path: &[T]) -> usize {
+        path.len()
+    }
+}
+
+struct IndirectInput<T>
+where
+    T: Keypad,
+{
+    next: T,
+}
+
+impl<K: Keypad> Keypad for IndirectInput<K> {
+    fn cost<T: Button>(&self, path: &[T]) -> usize {
+        iter::once(&T::start())
+            .chain(path.iter())
+            .tuple_windows::<(_, _)>()
+            .map(|x| {
+                let from = x.0;
+                let to = x.1;
+                let paths = from_to(from, to);
+                paths.iter().map(|path| self.next.cost(path)).min().unwrap()
+            })
+            .sum()
+    }
+}
+
+fn numeric_part(code: &[NumericKeypadButton]) -> usize {
     let numeric_part = code
         .iter()
         .filter_map(|button| match button {
@@ -164,8 +159,7 @@ fn complexity(commands: &[Command], code: &[NumericKeypadButton]) -> usize {
             NumericKeypadButton::Activate => None,
         })
         .fold(0, |acc, digit| acc * 10 + digit as usize);
-    let length = commands.len();
-    numeric_part * length
+    numeric_part
 }
 
 fn parse_code(code: &str) -> Vec<NumericKeypadButton> {
@@ -176,56 +170,27 @@ fn parse_code(code: &str) -> Vec<NumericKeypadButton> {
         })
         .collect()
 }
+
 fn main() {
-    let input = include_str!("../input/demo.txt");
+    let input = include_str!("../input/input.txt");
     let codes = input.lines().map(parse_code).collect::<Vec<_>>();
 
-    let commands = codes
-        .iter()
-        .map(|code| Keypad::enter_code(&Keypad::enter_code(&Keypad::enter_code(code))))
-        .collect::<Vec<_>>();
-    let complexities = commands
-        .iter()
-        .zip(codes)
-        .map(|(commands, code)| complexity(commands, &code))
-        .collect::<Vec<_>>();
-    let sum = complexities.iter().sum::<usize>();
-    println!("The sum of the complexities is {}.", sum);
-}
+    let numeric_keypad = IndirectInput {
+        next: IndirectInput {
+            next: IndirectInput {
+                next: DirectInput {},
+            },
+        },
+    };
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test() {
-        let testcases = [
-            (
-                "029A",
-                "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A",
-            ),
-            (
-                "980A",
-                "<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A",
-            ),
-            (
-                "179A",
-                "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
-            ),
-            (
-                "456A",
-                "<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A",
-            ),
-            (
-                "379A",
-                "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
-            ),
-        ];
-        for (code, result) in testcases {
-            let code = parse_code(code);
-            let commands = Keypad::enter_code(&Keypad::enter_code(&Keypad::enter_code(&code)));
-            println!("{}", to_string(&commands));
-            assert_eq!(commands.len(), result.len());
-        }
-    }
+    let x = codes
+        .iter()
+        .map(|code| {
+            let cost = numeric_keypad.cost(code);
+            let numeric_part = numeric_part(code);
+            println!("cost: {cost} * numeric: {numeric_part}");
+            cost * numeric_part
+        })
+        .sum::<usize>();
+    println!("{}", x);
 }
