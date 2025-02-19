9ab734c5e8ded81fecd7dfb57bd7c2dc2c748d72
Pascal Wey <566223+Rumpel22@users.noreply.github.com>
2025-02-19 21:01:52 +0000

    Solution day 23, part 2

--- day_23/src/main.rs
+++ day_23/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
 type Computer = String;
-#[derive(Debug, Eq, PartialEq, Hash)]
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
 struct Group {
     computers: Vec<Computer>,
 }
@@ -14,9 +14,25 @@ impl Group {
         group.computers.sort();
         group
     }
+    fn password(&self) -> String {
+        self.computers.join(",")
+    }
+    fn len(&self) -> usize {
+        self.computers.len()
+    }
+    fn add(&mut self, computer: &Computer) {
+        self.computers.push(computer.to_string());
+        self.computers.sort();
+    }
+
+    fn intersect(&self, other: &Self) -> Self {
+        let mut computers = self.computers.clone();
+        computers.retain(|c| other.computers.contains(c));
+        Self::new(&computers)
+    }
 }
 type Connections = Vec<Group>;
-type Computers = HashMap<Computer, Vec<Computer>>;
+type Computers = HashMap<Computer, Group>;
 
 fn parse(input: &str) -> Connections {
     input
@@ -35,12 +51,12 @@ fn computers(connections: &[Group]) -> Computers {
         let b = &connection.computers[1];
         computers
             .entry(a.clone())
-            .and_modify(|list: &mut Vec<String>| list.push(b.clone()))
-            .or_insert(vec![b.clone()]);
+            .and_modify(|group: &mut Group| group.add(b))
+            .or_insert(Group::new(&[b.clone()]));
         computers
             .entry(b.clone())
-            .and_modify(|list: &mut Vec<String>| list.push(a.clone()))
-            .or_insert(vec![a.clone()]);
+            .and_modify(|group: &mut Group| group.add(a))
+            .or_insert(Group::new(&[a.clone()]));
     });
     computers
 }
@@ -52,15 +68,39 @@ fn main() {
 
     let mut sets = HashSet::new();
     for (computer, others) in computers.iter().filter(|(c, _)| c.starts_with('t')) {
-        for other in others {
+        for other in &others.computers {
             let x = computers.get(other).unwrap();
-            for y in x {
-                if others.contains(y) {
+            for y in &x.computers {
+                if others.computers.contains(y) {
                     let group = Group::new(&[computer.clone(), other.clone(), y.clone()]);
                     sets.insert(group);
                 }
             }
         }
     }
-    println!("{}", sets.len());
+    println!(
+        "There are {} sets with three connected computers, where at least one starts with a 't'.",
+        sets.len()
+    );
+
+    let mut longest_set = Group::new(&[]);
+    for (computer, group) in &computers {
+        let mut set = group.clone();
+        set.add(computer);
+
+        for other in &group.computers {
+            if !set.computers.contains(other) {
+                continue;
+            }
+            let others = computers.get(other).unwrap();
+
+            set = set.intersect(others);
+            set.add(other);
+        }
+
+        if set.len() > longest_set.len() {
+            longest_set = set;
+        }
+    }
+    println!("The largest group is {}.", longest_set.password())
 }
